a4c112d3747130f610a48b5a0442464b73972f04
Shady Boukhary <shadyboukhary@Shadys-MacBook-Pro.local>
2018-10-01 23:16:45 -0500

    added firebase auth provider and packages

---
+++ src/app/firebase.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+        apiKey: "AIzaSyAfIBizSb0L3F_Yu2fycJZJZESxVkTnoeA",
+        authDomain: "msu-candy-store.firebaseapp.com",
+        databaseURL: "https://msu-candy-store.firebaseio.com",
+        projectId: "msu-candy-store",
+        storageBucket: "msu-candy-store.appspot.com",
+        messagingSenderId: "100839463519"
+}
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -15,11 +15,17 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class LoginPage {
 
+  email: string;
+  password: string;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
+  }
+
+  login() {
+    
   }
 
 }
